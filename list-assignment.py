19ef1d884d74470ed4477cfb9b47e52fbb60d94d
ALEXANDER <kamsiazubuike18@gmail.com>
2024-12-04 23:37:58 -0800

    assignment

---
+++ list-assignment.py
@@ -0,0 +1,11 @@
+first_name = 'kamsi'
+last_name = 'Azubuike'
+middle_name = 'Alexander'
+matriculation_number  = 'BHU/24/04/05/0119'
+
+#list Assignment
+list = [1,2,3,4,5,6,7,8,9,10]
+sum=0
+for i in list:
+    list_sum=sum+i
+print("list_sum")
